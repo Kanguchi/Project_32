7932c934f33ff9f4d4cd65177faca86795a25588
KanoKit <kano.kitaguchi@gmail.com>
2020-09-20 13:15:32 -0400

    started code for game:

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var stand1, stand2;
+var block1;
+var back, backimg;
+
+function preload(){
+  backimg = loadImage("background_img.jpg");
+}
 function setup() {
+  
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  
+  back = createSprite(400, 200, 800, 400);
+  back.addImage(backimg);
+  back.scale = 1.35;
+
+  engine = Engine.create();
+  world = engine.world;
+
+  stand1 = new Ground(300, 330, 250, 15);
+  stand2 = new Ground(600, 200, 200, 15);
+  
+  block1 = new Box(200, 300);
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);
   drawSprites();
+
+  stand1.display();
+  stand2.display();
+  fill(255, 190, 51);
+  block1.display();
+
 }
